1135892f3aac90d6d21dbb8f301001a891550a31
RickytheGuy <rickythepsguy@gmail.com>
2024-01-23 00:04:48 -0700

    finished chessboard

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Arrays;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -22,6 +24,10 @@ public class ChessBoard {
         this.chessboard[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
 
+    public void addPieceUsingRowCol(int row, int col, ChessPiece piece){
+        this.chessboard[row][col] = piece;
+    }
+
     /**
      * Gets a chess piece on the chessboard
      *
@@ -38,6 +44,44 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        // Pawns
+        for (int i = 0; i < 8; i++) {
+            addPieceUsingRowCol(1, i, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+            addPieceUsingRowCol(6, i, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+        }
+        // Rooks
+        addPieceUsingRowCol(0, 0, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+        addPieceUsingRowCol(0, 7, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+        addPieceUsingRowCol(7, 0, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+        addPieceUsingRowCol(7, 7, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+        // Knights
+        addPieceUsingRowCol(0, 1, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        addPieceUsingRowCol(0, 6, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        addPieceUsingRowCol(7, 1, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+        addPieceUsingRowCol(7, 6, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+        // Bishops
+        addPieceUsingRowCol(0, 2, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        addPieceUsingRowCol(0, 5, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        addPieceUsingRowCol(7, 2, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+        addPieceUsingRowCol(7, 5, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+        // Queens
+        addPieceUsingRowCol(0, 3, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN));
+        addPieceUsingRowCol(7, 3, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN));
+        // Kings
+        addPieceUsingRowCol(0, 4, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING));
+        addPieceUsingRowCol(7, 4, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessBoard that = (ChessBoard) o;
+        return Arrays.deepEquals(chessboard, that.chessboard);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(chessboard);
     }
 }
