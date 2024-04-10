b3eb477622408914cf19a3c743f4ac4b53fcf97a
Louis Rosas <rickythepsguy@gmail.com>
2024-04-10 11:12:38 -0600

    removed server in client

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,10 +1,10 @@
 import client.Repl;
-import server.Server;
+//import server.Server;
 public class Main {
     public static void main(String[] args) {
-        Server s = new Server();
-        int p = s.run(0);
-        Repl repl = new Repl(p);
+//        Server s = new Server();
+//        int p = s.run(0);
+        Repl repl = new Repl(0);
         repl.loginScreen();
     }
 }
